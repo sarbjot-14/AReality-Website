f8f2c4388703a915010a4f34ab55102018a3ad6f
sarbjot-konectAI <sarbjot.s@konect.ai>
2023-07-11 14:32:13 -0700

    done landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,18 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['var(--font-sans)', ...fontFamily.sans]
+      },
+      colors: {
+        primary: '#22577a',
+        secondary: '#8FCB9B',
+        accent: '#80ed99',
+        neutral: '#fffafb'
+      },
+      backgourndColors: {
+        primary: '#22577a',
+        secondary: '#22577a',
+        accent: '#80ed99',
+        neutral: '#fffafb'
       }
     }
   },
